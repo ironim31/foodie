ea19d68362d9b79f47b1d1685c3a37583af7f1db
Nakamatsu Minori <minori.naka31@gmail.com>
2023-05-25 19:13:35 +0900

    非同期通信記入

---
+++ script.js
@@ -0,0 +1,24 @@
+$(function(){
+    // console.log('helo');
+
+    // ここから非同期通信
+    $.ajax({
+        url: 'アクセスするURL', //アクセスするURL
+        type: 'postかget',　　 //post or get
+        cache: false,        //cacheを使うか使わないかを設定
+        dataType:'json',     //data type script・xmlDocument・jsonなど
+        data:data,           //アクセスするときに必要なデータを記載      
+      })
+      .done(function(response) { 
+         //通信成功時の処理
+         //成功したとき実行したいスクリプトを記載
+      })
+      .fail(function(xhr) {  
+　　　　　 //通信失敗時の処理
+         //失敗したときに実行したいスクリプトを記載
+      })
+      .always(function(xhr, msg) { 
+　　　　　//通信完了時の処理
+        //結果に関わらず実行したいスクリプトを記載
+   });   
+})
